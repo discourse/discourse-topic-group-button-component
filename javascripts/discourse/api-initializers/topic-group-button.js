f93b5af742824adc3d9b4d82bfc885d89e99e62b
Jarek Radosz <jarek@cvx.dev>
2025-10-09 15:09:55 +0200

    DEV: Update linting (#38)

--- javascripts/discourse/api-initializers/topic-group-button.js
+++ javascripts/discourse/api-initializers/topic-group-button.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("0.11.1", (api) => {
+export default apiInitializer((api) => {
   const currentUser = api.getCurrentUser();
 
   api.registerTopicFooterButton({
